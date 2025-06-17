875940ae4f587dd3b65f894f3d9397d8734a591e
brysonckennedy <brysonckennedy@gmail.com>
2025-06-17 17:48:06 -0500

    Added player input for rotation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,14 +11,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # player inputs
+        player.update(dt)
+
+        # rendering
         screen.fill((0.0, 0.0, 0.0))
+        player.draw(screen)
         pygame.display.flip()
+
+        # update delta time
         dt = clock.tick(60) / 1000
 
 
